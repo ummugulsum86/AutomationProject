c68d7d96a77c6249a24980d6cfe8ea0e6060ef5c
Ummgulsum Koc <umgk86@gmail.com>
2020-10-13 20:06:20 +0200

    i have updated

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,12 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello class");
+
+
+        System.out.println("Hello class EU3");
+
+        //More changes are comings
+        //i am ready to commit this
 
 
     }
